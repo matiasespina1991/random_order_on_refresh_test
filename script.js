7ab98911df49dba98cee1536b1e1089a32e7bf28
Matias Espina <matiasespina1991@gmail.com>
2021-03-02 06:15:56 +0100

    code cleaned - starting js

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// function changeBackground(color) {
+//     document.body.style.background = color;
+//  }
+//  window.addEventListener("load", function() {changeBackground('red')});
+
+//List of elements element
+var elementsToBeSorted = document.getElementsByClassName('element');
+
+// function sortNumber() {
+//     alert (document.getElementsByClassName('.col1').style.backgroundColor);  
+// }
+
+// for (i = 0; i < cars.length; i++) {
+//     text += cars[i] + "<br>";
+//   }
+
+
+console.log(elementsToBeSorted)
