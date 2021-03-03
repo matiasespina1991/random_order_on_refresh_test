8f7469da6e2a157951118df3c7e487de8d1285f8
Matias Espina <matiasespina1991@gmail.com>
2021-03-03 07:57:58 +0100

    script finished

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
+var elementsNodeList = document.getElementsByClassName('element-wrapper');
+// var elementsArray = Array.prototype.slice.call(elementsNodeList)
+// var elementsArray = [...elementsNodeList]
 
-// function changeBackground(color) {
-//     document.body.style.background = color;
-//  }
-//  window.addEventListener("load", function() {changeBackground('red')});
-
-//List of elements element
-var elementsToBeSorted = document.getElementsByClassName('element');
-
-// function sortNumber() {
-//     alert (document.getElementsByClassName('.col1').style.backgroundColor);  
-// }
-
-// for (i = 0; i < cars.length; i++) {
-//     text += cars[i] + "<br>";
-//   }
-
-
-console.log(elementsToBeSorted)
+for (var i=0;i<elementsNodeList.length;i++){
+    var randomNumber = Math.floor(Math.random() * elementsNodeList.length);
+    elementsNodeList[i].style.order = randomNumber;
+}
