5c0b4d5279092d780ec8785df3ceb95aff36a098
Matias Espina <matiasespina1991@gmail.com>
2021-03-06 17:07:41 +0100

    shuffle button with shuffle function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var elementsNodeList = document.getElementsByClassName('element-wrapper');
 
+
+function shuffleElementsOrder() {
 for (var i=0;i<elementsNodeList.length;i++){
     var randomNumber = Math.floor(Math.random() * elementsNodeList.length);
     elementsNodeList[i].style.order = randomNumber;
 }
+}
+
 
